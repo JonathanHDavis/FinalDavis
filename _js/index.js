e88c8b582598a3e5ddb1886ba21c0f4de8d9ad0f
Jonathan Davis <jonathanherscheldavis@gmail.com>
2016-01-23 19:07:53 -0500

    schedule

---
+++ _js/index.js
@@ -0,0 +1,62 @@
+$('.Sunday').hover(
+	function() {
+		$('.Special1').removeClass('hidden1');
+	}, 
+	function () {
+		$('.Special1').addClass('hidden1');
+	}
+	);
+
+$('.Monday').hover(
+	function() {
+		$('.Special2').removeClass('hidden2');
+	}, 
+	function () {
+		$('.Special2').addClass('hidden2');
+	}
+	);
+
+$('.Tuesday').hover(
+	function() {
+		$('.Special3').removeClass('hidden3');
+	}, 
+	function () {
+		$('.Special3').addClass('hidden3');
+	}
+	);
+
+$('.Wednesday').hover(
+	function() {
+		$('.Special4').removeClass('hidden4');
+	}, 
+	function () {
+		$('.Special4').addClass('hidden4');
+	}
+	);
+
+$('.Thursday').hover(
+	function() {
+		$('.Special5').removeClass('hidden5');
+	}, 
+	function () {
+		$('.Special5').addClass('hidden5');
+	}
+	);
+
+$('.Friday').hover(
+	function() {
+		$('.Special6').removeClass('hidden6');
+	}, 
+	function () {
+		$('.Special6').addClass('hidden6');
+	}
+	);
+
+$('.Saturday').hover(
+	function() {
+		$('.Special7').removeClass('hidden7');
+	}, 
+	function () {
+		$('.Special7').addClass('hidden7');
+	}
+	);
